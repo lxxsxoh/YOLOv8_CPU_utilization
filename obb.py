0762439983847e8b9f1936bab76d7a7c66cc643c
lxxsxoh <136955006+lxxsxoh@users.noreply.github.com>
2024-07-18 14:55:52 +0900

    obb.py

--- obb.py
+++ obb.py
@@ -1,14 +1,15 @@
 from ultralytics import YOLO
 
 # Load the exported TensorRT model
-#tensorrt_model = YOLO("yolov8n-obb.engine", task='obb')
 model = YOLO("yolov8n-obb.pt", task='obb')
-model.to("cpu")
+model.export(format="engine", imgsz=640)
+#tensorrt_model = YOLO("yolov8n-obb.engine", task='obb')
+#model.to("cpu")
 
 
 # Load the image path
 image_path = "./dota_images"
 
-#result = tensorrt_model(image_path)
+#result = tensorrt_model.predict(source=image_path, device=0)
 result = model(image_path)
 
